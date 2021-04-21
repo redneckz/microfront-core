d3b929f7a3bc5c8c12f0b80174329c4bf881e02c
redneckz <aleksandrovav@intech.rshb.ru>
2021-04-21 17:46:22 +0300

    Fix microfront-core-react bundle size by excluding React and ReactDOM

--- packages/microfront-core-react/webpack.config.js
+++ packages/microfront-core-react/webpack.config.js
@@ -21,5 +21,21 @@ module.exports = {
             name: 'microfrontCoreReact',
             type: 'umd'
         }
+    },
+    externals: {
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react',
+            umd: 'react'
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom',
+            umd: 'react-dom'
+        }
     }
 };
