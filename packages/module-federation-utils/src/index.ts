30bd453916c56563449cc127ebbe7bd975baa734
redneckz <aleksandrovav@intech.rshb.ru>
2021-04-28 15:26:17 +0300

    Move styles isolation to the core library and introduce Zone.js as a mandatory and shared singleton

--- packages/module-federation-utils/src/index.ts
+++ packages/module-federation-utils/src/index.ts
@@ -1,4 +1,2 @@
 export type { ModuleFederationPluginOptions, RemoteURL } from './module-federation-options';
 export { moduleFederationOptions } from './module-federation-options';
-export { shareScope } from './share-scope';
-export { insertStyleSafely } from './insert-style-safely';
