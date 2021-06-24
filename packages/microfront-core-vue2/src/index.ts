a61981507793d8e08ff3c790113712316f8404e6
redneckz <aleksandrovav@intech.rshb.ru>
2021-06-24 11:45:38 +0300

    Fix anomaly

--- packages/microfront-core-vue2/src/index.ts
+++ packages/microfront-core-vue2/src/index.ts
@@ -1,13 +1,12 @@
 import { PluginObject } from 'vue';
 
 import { shadow } from './shadow.directive';
-import { MicroFrontInShadow } from './microfront-in-shadow';
 import { MicroFrontContainer } from './microfront-container';
 
 const plugin: PluginObject<{}> = {
     install(Vue) {
         Vue.directive('shadow', shadow);
-        Vue.component('MicroFrontInShadow', MicroFrontInShadow);
+        Vue.component('MicroFrontInShadow', MicroFrontContainer);
         Vue.component('MicroFrontContainer', MicroFrontContainer);
     }
 };
@@ -17,6 +16,5 @@ export default plugin;
 
 // To allow individual component use, export components
 // each can be registered via Vue.component()
-export type { MicroFrontInShadowProps } from './microfront-in-shadow';
 export type { MicroFrontContainerProps } from './microfront-container';
-export { shadow, MicroFrontInShadow, MicroFrontContainer };
+export { shadow, MicroFrontContainer as MicroFrontInShadow, MicroFrontContainer };
